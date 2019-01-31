68f203f87ad953caaed620a60ece9758d04eb326
John Merrill <jmerrill@draper.com>
2019-01-31 15:00:33 -0500

    copy policy engine source when building pex kernel

--- runtime/isp_kernel.py
+++ runtime/isp_kernel.py
@@ -8,79 +8,87 @@ import glob
 import shutil
 import multiprocessing
 import argparse
+import sys
 import isp_utils
 
-# -- MAIN MODULE FUNCTIONALITY
-# arguments:
-#   policy: the name of the policy to compile
-#   policies_dir: root directory of policy definition. Example:
-#     Directory structure: /{x}/policies/osv/hifive/main.dpl
-#     policy name: osv.hifive.main
-#     policies_dir argument: /{x}/policies
-#   entities_dir: The location of the entities files for policies
-#   engine_dir: The policy engine repo path
-#   policy_out_dir: where should the compiled pex kernel be stored?
-#   debug: run policy tool with debug output
+def copyEngineSources(engine_dir, output_dir):
+    engine_output_dir = os.path.join(output_dir, "engine")
+    isp_utils.doMkDir(engine_output_dir)
 
-def build_policy_kernel(policy, policies_dir, entities_dir,
-                        engine_dir, policy_out_dir, debug):
-    policy_tool_cmd = "policy-tool"
-    policy_tool_args = ["-t", entities_dir, "-m", policies_dir, "-o", policy_out_dir] + [policy]
+    shutil.copytree(os.path.join(engine_dir, "validator"), os.path.join(engine_output_dir, "validator"))
+    shutil.copytree(os.path.join(engine_dir, "tagging_tools"), os.path.join(engine_output_dir, "tagging_tools"))
+    shutil.copy(os.path.join(engine_dir, "Makefile.isp"), engine_output_dir)
+    shutil.copy(os.path.join(engine_dir, "CMakeLists.txt"), engine_output_dir)
 
-    if debug is True:
-        policy_tool_args.insert(0, "-d")
-
-    shutil.rmtree(policy_out_dir, ignore_errors=True)
-    os.makedirs(policy_out_dir)
-
-    # faster if we trust cmake & don't clean, but that can leave you with stale .so file
-    fnull = open(os.devnull, 'w')
-    policy_engine_build_dir = os.path.join(engine_dir, "build")
-    subprocess.Popen(["make", "clean"], stdout=fnull, stderr=subprocess.STDOUT, cwd=policy_engine_build_dir).wait()
-
-    with open(os.path.join(policy_out_dir, "policy_tool.log"), "w+") as ptlog:
-        subprocess.Popen([policy_tool_cmd]+policy_tool_args, stdout=ptlog, stderr=subprocess.STDOUT, cwd=policy_out_dir).wait()
-
-    num_cores = str(multiprocessing.cpu_count())
-    with open(os.path.join(policy_out_dir, "build.log"), "w+") as buildlog:
-        subprocess.Popen(["make", "-j"+num_cores], stdout=buildlog, stderr=subprocess.STDOUT, cwd=policy_engine_build_dir).wait()
+    shutil.copytree(os.path.join(engine_dir, "soc_cfg"), os.path.join(output_dir, "soc_cfg"))
 
 
-def install_policy(policy, out_dir, policies_dir, entities_dir, validator_path, soc_cfg_path):
-    shutil.rmtree(out_dir, ignore_errors=True)
-    isp_utils.doMkDir(out_dir)
+def copyPolicyYaml(policy, policies_dir, entities_dir, output_dir):
+    policy_files = os.listdir(policies_dir)
+    for policy_file in policy_files:
+        if "yml" in policy_file or "log" in policy_file:
+            shutil.copy(os.path.join(policies_dir, policy_file), output_dir)
 
-    shutil.copy(validator_path, out_dir)
-
-    shutil.copytree(soc_cfg_path, os.path.join(out_dir, "soc_cfg"))
-
-    f_names = os.listdir(policies_dir)
-    for fn in f_names:
-        if "yml" in fn or "log" in fn:
-            shutil.copy(os.path.join(policies_dir, fn), out_dir)
-
-    entFile = os.path.join(entities_dir, policy + ".entities.yml")
-    destEnt = os.path.join(out_dir, policy + ".entities.yml")
+    entities_source = os.path.join(entities_dir, policy + ".entities.yml")
+    entities_dest = os.path.join(output_dir, policy + ".entities.yml")
 
     # special handling for composite policies
     # TODO: better way to determine composite policy?
     policy_parts = policy.split(".")[-1].split("-")
     policy_prefix = policy.rsplit(".", 1)[0] + "."
 
-    if os.path.isfile(entFile):
-        shutil.copy(entFile, out_dir)
+    if os.path.isfile(entities_source):
+        shutil.copy(entities_source, output_dir)
 
     # build composite entities for composite policy w.o existing entities
     elif (len(policy_parts) != 1):
-        shutil.copyfile(os.path.join(entities_dir, "empty.entities.yml"), destEnt)
-        with open(destEnt, 'wb') as comp_ents:
+        shutil.copyfile(os.path.join(entities_dir, "empty.entities.yml"), entities_dest)
+        with open(entities_dest, 'wb') as comp_ents:
             for p in policy_parts:
-                polEntFile = policy_prefix + p + ".entities.yml"
-                if os.path.isfile(os.path.join(entities_dir, polEntFile)):
-                    with open(os.path.join(entities_dir, polEntFile), 'rb') as fd:
+                policy_entities_file = policy_prefix + p + ".entities.yml"
+                if os.path.isfile(os.path.join(entities_dir, policy_entities_file)):
+                    with open(os.path.join(entities_dir, policy_entities_file), 'rb') as fd:
                         shutil.copyfileobj(fd, comp_ents);
     else:
-        shutil.copyfile(os.path.join(entities_dir, "empty.entities.yml"), destEnt)
+        shutil.copyfile(os.path.join(entities_dir, "empty.entities.yml"), entities_dest)
+ 
+def runPolicyTool(policy, policies_dir, entities_dir, output_dir):
+    policy_output_dir = os.path.join(output_dir, "engine", "policy")
+    policy_tool_cmd = "policy-tool"
+    policy_tool_args = ["-t", entities_dir, "-m", policies_dir, "-o", policy_output_dir, policy]
+
+    shutil.rmtree(policy_output_dir, ignore_errors=True)
+    os.makedirs(policy_output_dir)
+
+    with open(os.path.join(output_dir, "policy_tool.log"), "w+") as ptlog:
+        subprocess.Popen([policy_tool_cmd]+policy_tool_args, stdout=ptlog, stderr=subprocess.STDOUT, cwd=output_dir).wait()
+
+    if not os.path.isdir(policy_output_dir):
+        return False
+    
+    for policy_yaml in glob.glob(r"{}/policy_*.yml".format(policy_output_dir)):
+        shutil.copy(policy_yaml, output_dir)
+    
+    return True
+
+def buildPolicyKernel(policy, policies_dir, entities_dir, output_dir, debug):
+    engine_output_dir = os.path.join(output_dir, "engine")
+
+    if debug is True:
+        policy_tool_args.insert(0, "-d")
+
+    num_cores = str(multiprocessing.cpu_count())
+    with open(os.path.join(output_dir, "build.log"), "w+") as buildlog:
+        subprocess.Popen(["make", "-j"+num_cores, "-f", "Makefile.isp"], stdout=buildlog, stderr=subprocess.STDOUT, cwd=engine_output_dir).wait()
+
+    validator_path = os.path.join(engine_output_dir, "build", "librv32-renode-validator.so")
+    if not os.path.isfile(validator_path):
+        return False
+
+    shutil.move(validator_path, output_dir)
+    shutil.rmtree(engine_output_dir)
+
+    return True
 
 
 def main():
@@ -99,30 +107,24 @@ def main():
 
     isp_prefix = os.environ["ISP_PREFIX"]
     policies_dir = os.path.join(isp_prefix, "sources", "policies")
-
     engine_dir = os.path.join(isp_prefix, "sources", "policy-engine")
     policy_out_dir = os.path.join(engine_dir, "policy")
-    validator = "librv32-renode-validator.so"
-    validator_path = os.path.join(engine_dir, "build", validator)
     soc_cfg_path = os.path.join(engine_dir, "soc_cfg")
-
     entities_dir = os.path.join(policies_dir, "entities")
+    output_dir = os.path.abspath(os.path.join(args.output_dir, args.policy))
 
-    output_dir = os.path.join(args.output_dir, args.policy)
+    shutil.rmtree(output_dir, ignore_errors=True)
+    isp_utils.doMkDir(output_dir)
 
-    build_policy_kernel(args.policy,
-                        policies_dir,
-                        entities_dir,
-                        engine_dir,
-                        policy_out_dir,
-                        args.debug)
+    copyEngineSources(engine_dir, output_dir)
+    copyPolicyYaml(args.policy, policies_dir, entities_dir, output_dir)
+    if runPolicyTool(args.policy, policies_dir, entities_dir, output_dir) is False:
+        print("Failed to run policy tool")
+        sys.exit(1)
 
-    install_policy(args.policy,
-                   output_dir,
-                   policy_out_dir,
-                   entities_dir,
-                   validator_path,
-                   soc_cfg_path)
+    if buildPolicyKernel(args.policy, policies_dir, entities_dir, output_dir, args.debug) is False:
+        print("Failed to build policy kernel")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
