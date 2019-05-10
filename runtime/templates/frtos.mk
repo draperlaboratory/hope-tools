ff0cfac26645a57deed68f37bd4db09fbbaedb28
John Merrill <jmerrill@draper.com>
2019-05-10 08:47:49 -0400

    Fix FreeRTOS parallel build

--- runtime/templates/frtos.mk
+++ runtime/templates/frtos.mk
@@ -20,7 +20,7 @@ ISP_INCLUDES += -I$(ISP_RUNTIME)
 ISP_LIBS := $(FREERTOS_BUILD_DIR)/libfreertos.a
 
 RISCV_PATH 		?= $(ISP_PREFIX)
-RISCV_GCC     ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-gcc)
+RISCV_GCC     ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-gcc))
 RISCV_GXX     ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-g++)
 RISCV_OBJDUMP ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-objdump)
 RISCV_GDB     ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-gdb)
