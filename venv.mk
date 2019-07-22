c4878ed8f7011ec86e84d447dd10e2bc7ad68b68
John Merrill <jmerrill@draper.com>
2019-07-22 16:12:57 -0400

    Fix Makefile dependencies and add modules target

--- venv.mk
+++ venv.mk
@@ -6,6 +6,8 @@ VENV_DONE := $(VENV_DIR)/.done
 PYTHON_REQUIREMENTS := $(ROOT_DIR)/python-requirements.txt
 VENV = bash -c "source $(VENV_DIR)/bin/activate" &&
 
+all:
+
 $(VENV_DONE): $(PYTHON_REQUIREMENTS)
 	rm -rf $(VENV_DIR)
 	virtualenv -p $(PYTHON) $(VENV_DIR)
@@ -15,5 +17,3 @@ $(VENV_DONE): $(PYTHON_REQUIREMENTS)
 clean-venv:
 	rm -rf $(VENV_DIR)
 .PHONY: clean-venv
-
-all:
