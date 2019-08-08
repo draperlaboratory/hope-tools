35f7595085a2bdd82ee8d34f9d82b2e7e1051974
Baltazar Ortiz <baltazar@mit.edu>
2019-08-08 14:48:27 -0400

    remove extra lines from sel4.mk

--- runtime/templates/sel4.mk
+++ runtime/templates/sel4.mk
@@ -3,11 +3,6 @@ ISP_PREFIX ?= $(HOME)/.local/isp/
 ISP_RUNTIME := $(basename $(shell echo $(abspath $(MAKEFILE_LIST)) | grep -o " /.*/isp-runtime-sel4\.mk"))
 SEL4_DIR := $(ISP_PREFIX)/sel4test
 
-FREERTOS_RVDEMO_DIR := $(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio
-
-ISP_INCLUDES += -I$(ISP_PREFIX)/riscv32-unknown-elf/include
-ISP_INCLUDES += -I$(ISP_RUNTIME)
-
 RISCV_PATH 		?= $(ISP_PREFIX)
 RISCV_GCC     ?= $(abspath $(RISCV_PATH)/bin/clang)
 RISCV_GXX     ?= $(abspath $(RISCV_PATH)/bin/clang)
@@ -20,9 +15,3 @@ CC=$(RISCV_GCC)
 all:
 	bash $(SEL4_DIR)/init-build.sh -DPLATFORM=spike -DRISCV32=TRUE
 	ninja
-
-$(ISP_LIBS):
-	$(MAKE) -C $(FREERTOS_RVDEMO_DIR) lib
-
-.PHONY: isp-runtime-common
-isp-runtime-common: $(ISP_LIBS) $(ISP_OBJECTS)
