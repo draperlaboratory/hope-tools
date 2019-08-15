5c38fb94e9f80b3462a95606e81c2cad4e8abbc7
Baltazar Ortiz <baltazar@mit.edu>
2019-08-15 15:24:13 -0400

    Fix accidental usage of old policy dir function

--- runtime/isp_run.py
+++ runtime/isp_run.py
@@ -44,16 +44,14 @@ def runSim(exe_path, policy_dir, run_dir, sim, runtime, rule_cache, gdb, tag_onl
         return retVals.NO_BIN
 
     if "stock_" not in runtime and use_validator == True:
-        if not os.path.isdir(policy):
-            policy_dir = getPolicyDir(policy, kernels_dir)
-            if policy_dir is None:
-                logger.error("Failed to find directory for policy")
+        if not os.path.isdir(policy_dir):
+            if compileMissingPolicy(policy_dir) is False:
                 return retVals.NO_POLICY
 
     doMkDir(run_dir)
 
     if "stock_" not in runtime and use_validator == True:
-        doValidatorCfg(policy_dir, run_dir, exe_name, rule_cache)
+        doValidatorCfg(policy_dir, run_dir, exe_name, rule_cache, soc_cfg)
         doEntitiesFile(run_dir, exe_name)
         generateTagInfo(exe_path, run_dir, policy_dir)
 
