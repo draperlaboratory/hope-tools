179bc58fae4a3da9dadceddd3c9e24f1f4288ae4
Baltazar Ortiz <baltazar@mit.edu>
2019-08-28 16:21:44 -0400

    Clean up sel4 templates
    seL4 doesn't build with clang for now, so sel4.mk and stock_sel4.mk are
    identical.

--- runtime/templates/sel4.mk
+++ runtime/templates/sel4.mk
@@ -1,18 +1,17 @@
-ISP_PREFIX ?= $(HOME)/.local/isp/
+ISP_PREFIX  ?= $(HOME)/.local/isp/
+STOCK_TOOLS ?= $(abspath $(ISP_PREFIX)/stock-tools)
 
 ISP_RUNTIME := $(basename $(shell echo $(abspath $(MAKEFILE_LIST)) | grep -o " /.*/isp-runtime-sel4\.mk"))
+# Currently, seL4 doesn't build with clang, so we just have to build
+# with the stock RISC-V toolchain. This means that stock and normal seL4
+# builds are identical for now.
 
-RISCV_PATH    ?= $(ISP_PREFIX)
-RISCV_CLANG   ?= $(abspath $(RISCV_PATH)/bin/clang)
-RISCV_GXX     ?= $(RISCV_CLANG)
-RISCV_OBJDUMP ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-objdump)
-RISCV_GDB     ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-gdb)
-RISCV_AR      ?= $(abspath $(RISCV_PATH)/bin/riscv32-unknown-elf-ar)
-
-CC=$(RISCV_CLANG)
+RISCV_PATH    ?= $(STOCK_TOOLS)
+# riscv32/bin should contain a stock RISC-V GNU toolchain supporting
+# rv32ima / ilp32.
+RISCV_PREFIX ?= $(abspath $(RISCV_PATH)/riscv32/bin/riscv32-unknown-elf-)
 
 sel4-build: sel4-lib
 	rm $(OBJECTS)
 	mv target.a hope-seL4-app-template/projects/bootstrap_main/src/target.a
-	cd hope-seL4-app-template; bash ./make-riscv-build.sh -b 32 -p spike
-	cd build_sel4; bash ../hope-seL4/init-build.sh -DPLATFORM=spike -DRISCV32=TRUE; ninja
+	cd hope-seL4-app-template; bash ./make-riscv-build.sh -b 32 -p spike -C $(RISCV_PREFIX)
