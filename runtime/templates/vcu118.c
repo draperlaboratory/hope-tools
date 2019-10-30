e211b561002f4e72663cbcf590e45e4ff5cad87e
John Merrill <jmerrill@draper.com>
2019-10-30 16:47:50 -0400

    Add vcu118 bsp code

--- runtime/templates/vcu118.c
+++ runtime/templates/vcu118.c
@@ -2,8 +2,27 @@
 #include <stdarg.h>
 #include "isp_utils.h"
 
+extern int isp_main(void);
+
+void isp_test_device_pass(void)
+{
+  return;
+}
+
+void isp_test_device_fail(void)
+{
+  return;
+}
+
+uint32_t isp_get_time_usec()
+{
+  return 0;
+}
+
 int t_printf(const char *s, ...)
 {
+  va_list vl;
+
   va_start(vl, s);
   printf(s, vl);
   va_end(vl);
