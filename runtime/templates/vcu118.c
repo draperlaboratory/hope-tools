911e71ed4862ef74dde9528266fbd3ceb15814f4
John Merrill <jmerrill@draper.com>
2019-12-16 10:55:20 -0500

    Write to test address for vcu118

--- runtime/templates/vcu118.c
+++ runtime/templates/vcu118.c
@@ -2,14 +2,21 @@
 #include <stdarg.h>
 #include "isp_utils.h"
 
+#define VCU118_TEST_ADDR 0xa0000000
+
+#define VCU118_TEST_FAIL 0x0
+#define VCU118_TEST_PASS 0x1
+
 void isp_test_device_pass(void)
 {
-  return;
+  volatile uint32_t *test_device = (uint32_t *)VCU118_TEST_ADDR;
+  *test_device = VCU118_TEST_PASS;
 }
 
 void isp_test_device_fail(void)
 {
-  return;
+  volatile uint32_t *test_device = (uint32_t *)VCU118_TEST_ADDR;
+  *test_device = VCU118_TEST_FAIL;
 }
 
 uint32_t isp_get_time_usec()
