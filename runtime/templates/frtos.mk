23912dac373f4c7922a105559a9522ff4e1c080f
John Merrill <jmerrill@draper.com>
2019-12-20 16:11:54 -0500

    Adjust FreeRTOS Makefile for changed install structure

--- runtime/templates/frtos.mk
+++ runtime/templates/frtos.mk
@@ -2,18 +2,17 @@ ISP_PREFIX ?= $(HOME)/.local/isp/
 
 ISP_RUNTIME := $(basename $(shell echo $(abspath $(MAKEFILE_LIST)) | grep -o " /.*/isp-runtime-frtos\.mk"))
 FREERTOS_DIR := $(ISP_PREFIX)/FreeRTOS
-FREERTOS_RVDEMO_DIR := $(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio
-SDK_DIR := $(FREERTOS_RVDEMO_DIR)/freedom-e-sdk
-FREERTOS_BUILD_DIR := $(FREERTOS_RVDEMO_DIR)/build
+FREERTOS_INCLUDE_DIR := $(FREERTOS_DIR)/include
+FREERTOS_LIB_DIR := $(FREERTOS_DIR)/lib
 
-include $(FREERTOS_RVDEMO_DIR)/BuildEnvironment.mk
+include $(FREERTOS_DIR)/BuildEnvironment.mk
 
-ISP_INCLUDES := -I$(FREERTOS_DIR)/Source/include
-ISP_INCLUDES += -I$(FREERTOS_DIR)/Source/portable/GCC/RISC-V
-ISP_INCLUDES += -I$(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio
-ISP_INCLUDES += -I$(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/include
-ISP_INCLUDES += -I$(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env
-ISP_INCLUDES += -I$(FREERTOS_DIR)/Demo/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env/freedom-e300-hifive1
+ISP_INCLUDES := -I$(FREERTOS_INCLUDE_DIR)/Source/include
+ISP_INCLUDES += -I$(FREERTOS_INCLUDE_DIR)/Source/portable/GCC/RISC-V
+ISP_INCLUDES += -I$(FREERTOS_INCLUDE_DIR)/RISC-V-Qemu-sifive_e-FreedomStudio
+ISP_INCLUDES += -I$(FREERTOS_INCLUDE_DIR)/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/include
+ISP_INCLUDES += -I$(FREERTOS_INCLUDE_DIR)/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env
+ISP_INCLUDES += -I$(FREERTOS_INCLUDE_DIR)/RISC-V-Qemu-sifive_e-FreedomStudio/freedom-e-sdk/env/freedom-e300-hifive1
 ISP_INCLUDES += -I$(ISP_PREFIX)/riscv32-unknown-elf/include
 ISP_INCLUDES += -I$(ISP_RUNTIME)
 
@@ -25,19 +24,11 @@ ISP_OBJECTS      := $(patsubst %.c,%.o,$(ISP_C_SRCS))
 ISP_OBJECTS      += $(patsubst %.S,%.o,$(ISP_ASM_SRCS))
 
 ISP_LDFLAGS      += -lisp -L$(ISP_RUNTIME)
-ISP_LDFLAGS      += -lfreertos -L$(FREERTOS_BUILD_DIR)
+ISP_LDFLAGS      += -lfreertos -L$(FREERTOS_LIB_DIR)
 
 LIBISP           := $(ISP_RUNTIME)/libisp.a
+LIBFREERTOS 		 := $(FREERTOS_LIB_DIR)/libfreertos.a
 
-MALLOC_VERSION ?= heap_2
-
-ifeq ($(MALLOC_VERSION), heap_2)
-LIBFREERTOS := $(FREERTOS_BUILD_DIR)/libfreertos.a
-else
-LIBFREERTOS := $(FREERTOS_BUILD_DIR)/libfreertos-$(MALLOC_VERSION).a
-endif
-
-LIBISP := $(ISP_RUNTIME)/libisp.a
 ISP_LIBS := $(LIBFREERTOS)
 ISP_LIBS += $(LIBISP)
 
@@ -52,14 +43,6 @@ CC=$(RISCV_CLANG)
 
 all:
 
-$(LIBFREERTOS):
-	@echo $(MALLOC_VERSION)
-ifeq ($(MALLOC_VERSION), heap_2)
-	$(MAKE) -C $(FREERTOS_RVDEMO_DIR) lib
-else
-	$(MAKE) -C $(FREERTOS_RVDEMO_DIR) build/libfreertos-$(MALLOC_VERSION).a
-endif
-
 $(LIBISP): $(ISP_OBJECTS)
 	$(RISCV_AR) rcs $@ $(ISP_OBJECTS)
 
@@ -67,4 +50,4 @@ $(ISP_RUNTIME)/%.o: $(ISP_RUNTIME)/%.c
 	$(CC) $(ISP_CFLAGS) $(ISP_INCLUDES) -c $< -o $@
 
 .PHONY: isp-runtime-common
-isp-runtime-common: $(ISP_LIBS) $(ISP_OBJECTS)
+isp-runtime-common: $(LIBISP) $(ISP_OBJECTS)
