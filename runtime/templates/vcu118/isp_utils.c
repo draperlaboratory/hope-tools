b8213af25b5c2039767687f7fe14aedd0ba58e05
Arun Thomas <arun@draper.com>
2020-01-15 15:18:37 -0500

    WIP: tohost

--- runtime/templates/vcu118/isp_utils.c
+++ runtime/templates/vcu118/isp_utils.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include "isp_utils.h"
@@ -7,16 +8,22 @@
 #define VCU118_TEST_FAIL 0x3333
 #define VCU118_TEST_PASS 0x5555
 
+extern volatile uint64_t tohost;
+
 void isp_test_device_pass(void)
 {
   volatile uint32_t *test_device = (uint32_t *)VCU118_TEST_ADDR;
   *test_device = VCU118_TEST_PASS;
+  tohost = 0x01;
+  while (1);
 }
 
 void isp_test_device_fail(void)
 {
   volatile uint32_t *test_device = (uint32_t *)VCU118_TEST_ADDR;
   *test_device = VCU118_TEST_FAIL;
+  tohost = 0x11;
+  while (1);
 }
 
 uint32_t isp_get_time_usec()
