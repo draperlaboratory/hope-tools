d7f4b02ccc704daa1de38d6856acdba2f64a56e8
John Merrill <jmerrill@draper.com>
2020-01-29 16:28:45 -0500

    Move timer logic into isp_utils

--- runtime/templates/isp_utils.h
+++ runtime/templates/isp_utils.h
@@ -1,7 +1,10 @@
 #ifndef ISP_UTILS_H
 #define ISP_UTILS_H
 
+#include <stdint.h>
+
 int t_printf(const char *s, ...);
 int isp_main(void);
+uint32_t isp_get_time_usec(void);
 
 #endif // ISP_UTILS_H
