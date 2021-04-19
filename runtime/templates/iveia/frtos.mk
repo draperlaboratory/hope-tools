aa3febd4c18ea6b30531d428a7a96ff162d1b6bf
Silviu Chiricescu <silviu@draper.com>
2021-04-19 15:29:38 -0400

    * Template for iveia

---
+++ runtime/templates/iveia/frtos.mk
@@ -0,0 +1,77 @@
+ISP_PREFIX       ?= $(HOME)/.local/isp/
+ARCH ?= rv32
+ARCH_XLEN = $(subst rv,,$(ARCH))
+
+ISP_RUNTIME      := $(basename $(shell echo $(abspath $(MAKEFILE_LIST)) | grep -o " /.*/isp-runtime-frtos\.mk"))
+
+FREERTOS_DIR := $(ISP_PREFIX)/FreeRTOS
+FREERTOS_INCLUDE_DIR := $(FREERTOS_DIR)/include
+FREERTOS_LIB_DIR := $(FREERTOS_DIR)/lib
+
+LINKER_SCRIPT := $(FREERTOS_DIR)/build/iveia/link.ld
+
+ISP_HEADERS      += $(wildcard $(ISP_RUNTIME)/*.h)
+ISP_C_SRCS       += $(wildcard $(ISP_RUNTIME)/*.c)
+ISP_ASM_SRCS     += $(wildcard $(ISP_RUNTIME)/*.S)
+
+ISP_OBJECTS      := $(patsubst %.c,%.o,$(ISP_C_SRCS))
+ISP_OBJECTS      += $(patsubst %.S,%.o,$(ISP_ASM_SRCS))
+
+ifneq ($(ARCH), rv64)
+ISP_CFLAGS			 := -march=rv32ima -mabi=ilp32 -mcmodel=medium --target=riscv32-unknown-elf
+else
+ISP_CFLAGS			 := -march=rv64imafd -mabi=lp64d -mcmodel=medany --target=riscv64-unknown-elf
+endif
+
+ISP_CFLAGS			 += -Wall -Wextra -O0 -g3 -std=gnu11 -mno-relax
+ISP_CFLAGS			 += -ffunction-sections -fdata-sections -fno-builtin-printf
+ISP_CFLAGS 			 += -Dmalloc\(x\)=pvPortMalloc\(x\) -Dfree\(x\)=vPortFree\(x\)
+
+# flag to initialize hardware if running on FPGA
+ISP_CFLAGS 			 += -DFPGA=1
+
+ISP_ASMFLAGS     := $(ISP_CFLAGS)
+
+ISP_INCLUDES 		 := -I$(FREERTOS_INCLUDE_DIR)/Source/include
+ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Source/portable/GCC/RISC-V
+ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/Common/include
+ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1
+ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1/bsp
+ISP_INCLUDES     += -I$(ISP_PREFIX)/clang_sysroot/riscv64-unknown-elf/include
+ISP_INCLUDES     += -I$(ISP_RUNTIME)
+
+RISCV_PATH			 ?= $(ISP_PREFIX)
+RISCV_CLANG			 ?= $(abspath $(RISCV_PATH)/bin/clang)
+RISCV_GXX			   ?= $(RISCV_CLANG)
+RISCV_OBJDUMP		 ?= $(abspath $(RISCV_PATH)/bin/llvm-objdump)
+RISCV_GDB				 ?= $(abspath $(RISCV_PATH)/bin/riscv64-unknown-elf-gdb)
+RISCV_AR				 ?= $(abspath  $(RISCV_PATH)/bin/llvm-ar)
+
+CC 							 := $(RISCV_CLANG)
+
+LIBISP           := $(ISP_RUNTIME)/libisp.a
+
+ISP_LIBS         := $(LIBISP)
+
+ISP_LDFLAGS			 := -T $(LINKER_SCRIPT) -nostartfiles -defsym=_STACK_SIZE=4K -fuse-ld=lld
+ISP_LDFLAGS  		 += -Wl,--gc-sections
+
+ISP_LDFLAGS      += -lfreertos-iveia$(ARCH_XLEN) -L$(FREERTOS_LIB_DIR) -fuse-ld=lld
+ISP_LDFLAGS      += -lisp -L$(ISP_RUNTIME)
+ISP_LDFLAGS      += -Wl,--wrap=puts
+ISP_LDFLAGS      += -Wl,--wrap=printf
+
+all:
+
+$(LIBISP): $(ISP_OBJECTS)
+	$(RISCV_AR) rcs $@ $(ISP_OBJECTS)
+
+$(ISP_RUNTIME)/%.o: $(ISP_RUNTIME)/%.c
+	$(CC) $(ISP_CFLAGS) $(ISP_INCLUDES) -c $< -o $@
+
+clean: isp-clean
+
+.PHONY: isp-clean isp-runtime-common
+isp-clean:
+
+isp-runtime-common: $(ISP_LIBS) $(ISP_OBJECTS)
---
+++ runtime/templates/iveia/isp_utils.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "isp_utils.h"
+
+#define CPU_CLOCK_HZ 50000000
+
+extern volatile uint64_t tohost;
+
+void write_tohost(uint64_t val)
+{
+  tohost = val;
+}
+
+void tohost_exit(uint64_t val)
+{
+  write_tohost(val << 1 | 1);
+}
+
+void isp_test_device_pass(void)
+{
+  tohost_exit(0);
+}
+
+void isp_test_device_fail(void)
+{
+  tohost_exit(0x10);
+}
+
+/**
+ * Capture the current 64-bit cycle count.
+ */
+uint64_t isp_get_cycle_count(uint32_t *result_hi, uint32_t *result_lo)
+{
+#if __riscv_xlen == 64
+	uint64_t cycle;
+	asm volatile(
+		"%=:\n\t"
+		"csrr %0, mcycle\n\t"
+		: "=r"(cycle));
+
+  return cycle;
+#else
+	uint32_t cycle_lo, cycle_hi, temp_hi;
+        // Loop outside of the inline assembly to avoid issues with LLVM
+        // policies that don't tag inline assembly
+        do {
+            asm volatile(
+                    "csrr %1, mcycleh\n\t"
+                    "csrr %0, mcycle\n\t"
+                    "csrr %2, mcycleh\n\t"
+                    : "=r"(cycle_lo), "=r"(cycle_hi), "=r" (temp_hi)
+                    : // No inputs.
+                    : // No temps
+                    );
+        } while (temp_hi != cycle_hi);
+
+   // XXX: pass back hi/lo to get around unreliable 64-bit intrinsics
+   if(result_hi != NULL) {
+     *result_hi = cycle_hi;
+   }
+   if(result_lo != NULL) {
+     *result_lo = cycle_lo;
+   }
+	 return (((uint64_t)cycle_hi) << 32) | (uint64_t)cycle_lo;
+#endif
+}
+
+/**
+ * Use `mcycle` counter to get usec resolution.
+ * On RV32 only, reads of the mcycle CSR return the low 32 bits,
+ * while reads of the mcycleh CSR return bits 63–32 of the corresponding
+ * counter.
+ * We convert the 64-bit read into usec. The counter overflows in roughly an hour
+ * and 20 minutes. Probably not a big issue though.
+ * At 50HMz clock rate, 1 us = 50 ticks
+ */
+uint32_t isp_get_time_usec(void)
+{
+	return (uint32_t)(isp_get_cycle_count(NULL, NULL) / (CPU_CLOCK_HZ / 1000000));
+}
+
+uint32_t isp_get_timer_freq()
+{
+  return CPU_CLOCK_HZ;
+}
+
+int t_printf(const char *s, ...)
+{
+  va_list vl;
+
+  va_start(vl, s);
+  printf(s, vl);
+  va_end(vl);
+
+  return 0;
+}
+
