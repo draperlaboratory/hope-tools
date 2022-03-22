fe40765af7c16d6561dee7e0f114552e1e98e71b
Amanda Strnad <astrnad@draper.com>
2022-03-22 10:20:18 -0400

    Reduce warnings when compiling tests

--- runtime/templates/isp_utils.h
+++ runtime/templates/isp_utils.h
@@ -7,5 +7,8 @@ int t_printf(const char *s, ...);
 int isp_main(void);
 uint64_t isp_get_cycle_count(uint32_t *result_hi, uint32_t *result_lo);
 uint32_t isp_get_time_usec(void);
+void isp_test_device_pass(void);
+void isp_test_device_fail(void);
+uint32_t isp_get_timer_freq(void);
 
 #endif // ISP_UTILS_H
