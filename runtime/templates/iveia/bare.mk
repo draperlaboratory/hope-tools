4a5daa2cc80df689429d0247ba85a355fcdd3844
Alec Roelke <aroelke@draper.com>
2023-05-16 14:56:36 -0400

    Add Iveia test files

--- runtime/templates/iveia/bare.mk
+++ runtime/templates/iveia/bare.mk
@@ -1,6 +1,6 @@
 ISP_PREFIX       ?= $(HOME)/.local/isp/
-ARCH ?= rv32
-ARCH_XLEN = $(subst rv,,$(ARCH))
+ARCH             ?= rv64
+ARCH_XLEN         = $(subst rv,,$(ARCH))
 
 ISP_RUNTIME      := $(basename $(shell echo $(abspath $(MAKEFILE_LIST)) | grep -o " /.*/isp-runtime-bare\.mk"))
 
@@ -11,62 +11,53 @@ ISP_ASM_SRCS     += $(wildcard $(ISP_RUNTIME)/*.S)
 ISP_OBJECTS      := $(patsubst %.c,%.o,$(ISP_C_SRCS))
 ISP_OBJECTS      += $(patsubst %.S,%.o,$(ISP_ASM_SRCS))
 
-ISP_CFLAGS			 += -Wall -Wextra -O0 -g3 -std=gnu11 -mno-relax
-ISP_CFLAGS			 += -ffunction-sections -fdata-sections -fno-builtin-printf
+ISP_CFLAGS       += -Wall -Wextra -O0 -g3 -std=gnu11 -mno-relax
+ISP_CFLAGS       += -ffunction-sections -fdata-sections -fno-builtin-printf
 ISP_INCLUDES     += -I$(ISP_PREFIX)/clang_sysroot/riscv64-unknown-elf/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/local/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/bsp/iveia/ap/include
 ISP_INCLUDES     += -I$(ISP_RUNTIME)
 
-RISCV_PATH			 ?= $(ISP_PREFIX)
-RISCV_CLANG			 ?= $(abspath $(RISCV_PATH)/bin/clang)
-RISCV_GXX			   ?= $(RISCV_CLANG)
-RISCV_OBJDUMP		 ?= $(abspath  $(RISCV_PATH)/bin/llvm-objdump)
-RISCV_GDB				 ?= $(abspath $(RISCV_PATH)/bin/riscv64-unknown-elf-gdb)
-RISCV_AR				 ?= $(abspath  $(RISCV_PATH)/bin/llvm-ar)
+RISCV_PATH       ?= $(ISP_PREFIX)
+RISCV_CLANG      ?= $(abspath $(RISCV_PATH)/bin/clang)
+RISCV_GXX        ?= $(RISCV_CLANG)
+RISCV_OBJDUMP    ?= $(abspath  $(RISCV_PATH)/bin/llvm-objdump)
+RISCV_GDB        ?= $(abspath $(RISCV_PATH)/bin/riscv64-unknown-elf-gdb)
+RISCV_AR         ?= $(abspath  $(RISCV_PATH)/bin/llvm-ar)
 
-ifneq ($(ARCH), rv64)
-RISCV_ARCH 			 ?= rv32ima
-RISCV_ABI        ?= ilp32
-RISCV_TARGET	?= riscv32-unknown-elf
-else
-RISCV_ARCH ?= rv64imafd
-RISCV_ABI ?= lp64d
-RISCV_TARGET ?= riscv64-unknown-elf
+RISCV_ARCH       ?= rv64imafd
+RISCV_ABI        ?= lp64d
+RISCV_TARGET     ?= riscv64-unknown-elf
 
-ISP_CFLAGS += -mcmodel=medany
-ISP_LDFLAGS += -mcmodel=medany
-endif
-CC 							 := $(RISCV_CLANG)
+ISP_CFLAGS       += -DRV64 -mcmodel=medany
+ISP_LDFLAGS      += -mcmodel=medany
 
-ISP_CFLAGS			 += -march=$(RISCV_ARCH) -mabi=$(RISCV_ABI) --target=$(RISCV_TARGET)
+CC               := $(RISCV_CLANG)
+
+ISP_CFLAGS       += -march=$(RISCV_ARCH) -mabi=$(RISCV_ABI) --target=$(RISCV_TARGET)
 
 ISP_ASMFLAGS     := $(ISP_CFLAGS)
 
 BSP_BASE         := $(ISP_RUNTIME)/bsp
-BSP_SRC          := $(BSP_BASE)/src
 
-LIBIVEIA				 := $(BSP_BASE)/libiveia.a
 LIBISP           := $(ISP_RUNTIME)/libisp.a
+ISP_LIBS         := $(LIBISP)
 
-ISP_LIBS         := $(LIBISP) $(LIBIVEIA)
-
-ISP_LDFLAGS      := -T $(BSP_SRC)/link.ld -nostartfiles -defsym=_STACK_SIZE=4K -fuse-ld=lld
-ISP_LDFLAGS			 += -Wl,--wrap=isatty
+ISP_LDFLAGS      := -T$(ISP_RUNTIME)/link.ld -nostartfiles -defsym=_STACK_SIZE=4K -fuse-ld=lld
+ISP_LDFLAGS      += -Wl,--wrap=isatty
 ISP_LDFLAGS      += -Wl,--wrap=printf
 ISP_LDFLAGS      += -Wl,--wrap=puts
 ISP_LDFLAGS      += -Wl,--wrap=read
 ISP_LDFLAGS      += -Wl,--wrap=write
 ISP_LDFLAGS      += -Wl,--wrap=malloc
 ISP_LDFLAGS      += -Wl,--wrap=free
-ISP_LDFLAGS 		 += -Wl,--undefined=pvPortMalloc
+ISP_LDFLAGS      += -Wl,--undefined=pvPortMalloc
 ISP_LDFLAGS      += -Wl,--undefined=pvPortFree
 
-ISP_LDFLAGS      += -liveia -L$(BSP_SRC)
+ISP_LDFLAGS      += -lbsp -L$(ISP_PREFIX)/bsp/iveia/ap/lib
 ISP_LDFLAGS      += -lisp -L$(ISP_RUNTIME)
-
-all:
-
-$(LIBIVEIA):
-	ARCH=$(ARCH) make -C $(BSP_SRC)
+ISP_LDFLAGS      += -L$(ISP_PREFIX)/local/lib/$(RISCV_ARCH)/$(RISCV_ABI)
 
 debug:
 	echo $(CC)
@@ -77,10 +68,13 @@ $(LIBISP): $(ISP_OBJECTS)
 $(ISP_RUNTIME)/%.o: $(ISP_RUNTIME)/%.c
 	$(CC) $(ISP_CFLAGS) $(ISP_INCLUDES) -c $< -o $@
 
+$(ISP_RUNTIME)/%.o: $(ISP_RUNTIME)/%.S
+	$(CC) $(ISP_ASMFLAGS) -c $< -o $@
+
 clean: isp-clean
 
 .PHONY: isp-clean isp-runtime-common
 isp-clean:
-	make -C $(BSP_SRC) clean
+	make -C $(BSP_BASE) clean
 
 isp-runtime-common: $(ISP_LIBS) $(ISP_OBJECTS)
--- runtime/templates/iveia/frtos.mk
+++ runtime/templates/iveia/frtos.mk
@@ -8,7 +8,7 @@ FREERTOS_DIR := $(ISP_PREFIX)/FreeRTOS
 FREERTOS_INCLUDE_DIR := $(FREERTOS_DIR)/include
 FREERTOS_LIB_DIR := $(FREERTOS_DIR)/lib
 
-LINKER_SCRIPT := $(FREERTOS_DIR)/build/iveia/link.ld
+LINKER_SCRIPT := $(FREERTOS_DIR)/build/vcu118/link.ld
 
 ISP_HEADERS      += $(wildcard $(ISP_RUNTIME)/*.h)
 ISP_C_SRCS       += $(wildcard $(ISP_RUNTIME)/*.c)
@@ -18,9 +18,9 @@ ISP_OBJECTS      := $(patsubst %.c,%.o,$(ISP_C_SRCS))
 ISP_OBJECTS      += $(patsubst %.S,%.o,$(ISP_ASM_SRCS))
 
 ifneq ($(ARCH), rv64)
-ISP_CFLAGS			 := -march=rv32ima -mabi=ilp32 -mcmodel=medium --target=riscv32-unknown-elf
+ISP_CFLAGS			 := -march=rv32ima -mabi=ilp32 -mcmodel=medium --target=riscv32-unknown-elf -DRV32
 else
-ISP_CFLAGS			 := -march=rv64imafd -mabi=lp64d -mcmodel=medany --target=riscv64-unknown-elf
+ISP_CFLAGS			 := -march=rv64imafd -mabi=lp64d -mcmodel=medany --target=riscv64-unknown-elf -DRV64
 endif
 
 ISP_CFLAGS			 += -Wall -Wextra -O0 -g3 -std=gnu11 -mno-relax
@@ -32,12 +32,15 @@ ISP_CFLAGS 			 += -DFPGA=1
 
 ISP_ASMFLAGS     := $(ISP_CFLAGS)
 
-ISP_INCLUDES 		 := -I$(FREERTOS_INCLUDE_DIR)/Source/include
-ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Source/portable/GCC/RISC-V
-ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/Common/include
-ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1
-ISP_INCLUDES 		 += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1/bsp
-ISP_INCLUDES     += -I$(ISP_PREFIX)/clang_sysroot/riscv64-unknown-elf/include
+ISP_INCLUDES     := -I$(ISP_PREFIX)/clang_sysroot/riscv64-unknown-elf/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/local/include
+ISP_INCLUDES     += -I$(ISP_PREFIX)/bsp/vcu118/ap/include
+ISP_INCLUDES     += -I$(FREERTOS_INCLUDE_DIR)/Source/include
+ISP_INCLUDES     += -I$(FREERTOS_INCLUDE_DIR)/Source/portable/GCC/RISC-V
+ISP_INCLUDES     += -I$(FREERTOS_INCLUDE_DIR)/Demo/Common/include
+ISP_INCLUDES     += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1
+ISP_INCLUDES     += -I$(FREERTOS_INCLUDE_DIR)/Demo/RISC-V_Galois_P1/bsp
 ISP_INCLUDES     += -I$(ISP_RUNTIME)
 
 RISCV_PATH			 ?= $(ISP_PREFIX)
@@ -56,7 +59,7 @@ ISP_LIBS         := $(LIBISP)
 ISP_LDFLAGS			 := -T $(LINKER_SCRIPT) -nostartfiles -defsym=_STACK_SIZE=4K -fuse-ld=lld
 ISP_LDFLAGS  		 += -Wl,--gc-sections
 
-ISP_LDFLAGS      += -lfreertos-iveia$(ARCH_XLEN) -L$(FREERTOS_LIB_DIR) -fuse-ld=lld
+ISP_LDFLAGS      += -lfreertos-vcu118$(ARCH_XLEN) -L$(FREERTOS_LIB_DIR) -fuse-ld=lld
 ISP_LDFLAGS      += -lisp -L$(ISP_RUNTIME)
 ISP_LDFLAGS      += -Wl,--wrap=puts
 ISP_LDFLAGS      += -Wl,--wrap=printf
